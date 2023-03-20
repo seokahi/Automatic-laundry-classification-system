98432bc03a16df49230f322a4383e538c75add16
seokahi <skh9797@naver.com>
2023-03-20 15:43:45 +0900

    chore : 이미지 학습

--- FASHION_MNIST.py
+++ FASHION_MNIST.py
@@ -1,38 +1,25 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-# 이미지 사이즈를 28x28로 조정합니다.
-img_height = 28
-img_width = 28
-
-# 데이터를 불러옵니다.
-train_data = ImageDataGenerator(rescale=1./255).flow_from_directory(
-    'underwear',
-    target_size=(img_height, img_width),
-    batch_size=32,
-    class_mode='categorical'
+import numpy as np
+import matplotlib.pyplot as plt
+MNIST_model = keras.models.load_model('custom.h5')
+img = tf.keras.utils.load_img(
+    './images/cc.png',
+    target_size=(28, 28),
+    color_mode='grayscale'
 )
+img_array = tf.keras.utils.img_to_array(img)
 
-# 모델을 생성합니다.
-model = keras.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu',
-                  input_shape=(img_height, img_width, 3)),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(128, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(1, activation='sigmoid')
-])
+image = np.expand_dims(img_array, axis=-1)
+image = np.expand_dims(img_array, axis=0)
+predictions = MNIST_model.predict(image)
+score = tf.nn.softmax(predictions[0])
 
-# 모델을 컴파일합니다.
-model.compile(optimizer='adam',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
+class_index = np.argmax(predictions[0])
+class_names = ['bra', 'pantie', 'pants', 'short pants', 'skirt', 'socks']
 
-# 모델을 학습합니다.
-model.fit(train_data, epochs=10)
+print(class_index)
+print('Predicted class:', class_names[class_index])
+
+plt.imshow(img)
+plt.show()ㄴ
