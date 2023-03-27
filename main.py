7aae1fb5bdd3b28de5e648052bd28bfa5ac5103c
seokahi <skh9797@naver.com>
2023-03-27 10:21:01 +0900

    feat : 데이터셋 추가

--- main.py
+++ main.py
@@ -19,32 +19,32 @@ img = cv2.imread(imageUrl)
 img = cv2.resize(img, dsize=(0, 0), fx=0.2, fy=0.2,
                  interpolation=cv2.INTER_LINEAR)
 
+rect = cv2.selectROI("location", img, False, False)
+
+
+cv2.destroyAllWindows()
+
+# GrabCut 실행을 위한 초기 마스크 생성
 mask = np.zeros(img.shape[:2], np.uint8)
 
+# ROI 영역과 그 외 영역을 지정
 bgdModel = np.zeros((1, 65), np.float64)
 fgdModel = np.zeros((1, 65), np.float64)
 
-rect = (53, 139, 538, 583)
+# GrabCut 알고리즘 실행
 cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
 
-mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
-img = img*mask2[:, :, np.newaxis]
+# 결과로부터 전경 마스크 생성
+mask2 = np.where((mask == cv2.GC_FGD) | (
+    mask == cv2.GC_PR_FGD), 255, 0).astype('uint8')
 
-tmp = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-_, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
-b, g, r = cv2.split(img)
-rgba = [b, g, r, alpha]
-dst = cv2.merge(rgba, 4)
+# 전경 마스크를 이미지에 적용하여 전경만 추출
+result = cv2.bitwise_and(img, img, mask=mask2)
 
-cv2.imwrite("result.png", dst)
-
-grabcut_image = cv2.imread('result.png')
-
-x_pos, y_pos, width, height = cv2.selectROI(
-    "location", grabcut_image, False, False)
-
-grabcut_image = grabcut_image[y_pos:y_pos+height, x_pos:x_pos+width]
-cv2.imshow("grabcut_image", grabcut_image)
+# 결과 출력
+cv2.imshow('result', result)
 cv2.waitKey(0)
-cv2.imwrite("grabcut_image.png", grabcut_image)
+cv2.destroyAllWindows()
+cv2.imwrite("result.png", result)
+
 
