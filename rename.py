33537dcdb6a834e7b34b27794388ed3d30282527
seokahi <skh9797@naver.com>
2023-03-28 11:15:06 +0900

    feat : 데이터셋 추가 & 이름 변경

--- rename.py
+++ rename.py
@@ -2,9 +2,9 @@ import os
 import glob
 
 # path = '0529/contract_image/short_sleeve'
-path = '0529/all/long_pants'
+path = 'dataset/dataset/skirt'
 # path = 'week13/modi_dataset/long_pants'
-save_path = '0529/dataset/long_pants/'
+save_path = 'dataset/dataset/skirt/'
 # save_path = '0529/all/short_sleeve/'
 
 cnt = len(os.listdir(save_path))
@@ -13,7 +13,7 @@ for i, f in enumerate(files):
     ftitle, fext = os.path.splitext(f)
     fname = ftitle.split('_')[0]  # 파일이름
     # 파일이름을 fname_0000.jpg 이런 식으로 변경
-    filename = save_path + 'long_pants' + '_' + '{0:04d}'.format(cnt) + fext
+    filename = save_path + '_' + '{0:04d}'.format(cnt) + fext
     print(filename)
     os.rename(f, filename)
     cnt = len(os.listdir(save_path))
