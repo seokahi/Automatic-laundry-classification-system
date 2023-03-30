896ddf36d453e25301865fdf98cca61017a5cf88
seokahi <skh9797@naver.com>
2023-03-30 19:14:06 +0900

    feat : 데이터셋 추가 & 이름 변경

--- rename.py
+++ rename.py
@@ -2,8 +2,8 @@ import os
 import glob
 import shutil
 
-path = 'dataset/dataset/skirt'
-save_path = 'dataset/dataset/skirt/'
+path = 'dataset/dataset/bra'
+save_path = 'dataset/dataset/bra/'
 
 cnt = len(os.listdir(save_path))
 files = glob.glob(path + '/*')
@@ -13,7 +13,7 @@ for i, f in enumerate(files):
     fname = ftitle.split('_')[0]  # 파일이름
 
     # 파일이름을 fname_0000.jpg 이런 식으로 변경
-    filename = save_path + 'skirt' + '_' + '{0:04d}'.format(cnt) + fext
+    filename = save_path + 'bras' + '_' + '{0:04d}'.format(cnt) + fext
     print(filename)
 
     # Use shutil.move to overwrite existing files
