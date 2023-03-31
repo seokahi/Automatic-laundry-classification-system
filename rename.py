7c8fab04a4da561f028c41686db8b4811eb2d477
seokahi <skh9797@naver.com>
2023-03-31 19:14:06 +0900

    feat : 데이터셋 이름 변경

--- rename.py
+++ rename.py
@@ -13,11 +13,11 @@ for i, f in enumerate(files):
     fname = ftitle.split('_')[0]  # 파일이름
 
     # 파일이름을 fname_0000.jpg 이런 식으로 변경
-    filename = save_path + 'bras' + '_' + '{0:04d}'.format(cnt) + fext
+    filename = save_path + 'bra' + '_' + '{0:04d}'.format(cnt) + fext
     print(filename)
 
     # Use shutil.move to overwrite existing files
     shutil.move(f, filename)
     cnt += 1
-
+ 
 print(cnt)
