98ec02e7696b41b9ae44b1120700155f87a2f368
seokahi <skh9797@naver.com>
2023-04-04 18:31:06 +0900

    chore : 데이터셋 추가 & 이름 변경

--- main.py
+++ main.py
@@ -54,4 +54,106 @@ cv2.destroyAllWindows()
 cv2.imwrite("result.png", result)
 cv2.imwrite("result2.png", result2)
 
+img = cv2.imread('result.png')
+image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+mask = cv2.inRange(image, (1, 1, 1), (255, 255, 255))
+image = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow("remove_black", image)
+
+# BGR로 된 그랩컷 이미지를 RGB로 바꿈.
+# image = cv2.cvtColor(grabcut_image_PIL, cv2.COLOR_BGR2RGB)
+
+img_np = np.asarray(image)
+# 이미지 height, width 통합 // kmeans 입력 데이터 조건 맞추기 위해 차원 변경
+
+image = img_np.reshape(img_np.shape[0] * img_np.shape[1], img_np.shape[2])
+# 3
+img = cv2.imread('result.png')
+cv2.imshow("Lenna", img)  # 불러온 이미지를 Lenna라는 이름으로 창 표시
+
+image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+image = image.reshape((image.shape[0] * image.shape[1], 3))  # height, width 통합
+
+mask = np.all(image != [0, 0, 0], axis=-1)  # 검은색(0, 0, 0)이 아닌 픽셀에 True 값 부여
+image = image[mask]
+
+# 군집형성의 개수를 3으로 설정
+k = 3
+# 학습의 동일한 결과를 위해 random_state를 10으로 고정
+# k평균 군집화 알고리즘 함수를 활용해 특정 군집 개수만큼 군집화 진행
+model = KMeans(n_clusters=k, random_state=10)
+# image에 대한 클러스터링 수행
+model.fit(image)
+
+# .cluster_centers_ 라는 함수를 통해서 중심점 확인
+for center in model.cluster_centers_:
+    print(center)
+
+# 중심점 통해비율 구하는 함수
+
+
+def centroid_histogram(model):
+    numLabels = np.arange(0, len(np.unique(model.labels_)) + 1)
+    (hist, _) = np.histogram(model.labels_, bins=numLabels)
+
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    return hist
+
+
+hist = centroid_histogram(model)
+print("색상 비율 출력 => ", hist)
+
+# 가장 많은 비율 차지하고 있는 index 추출
+max_index = hist.argmax()
+
+# bar 그려주기 위한 함수
+
+
+def plot_colors(hist, centroids):
+    bar = np.zeros((12, 36, 3), dtype="uint8")
+    startX = 0
+    for (percent, color) in zip(hist, centroids):
+        # plot the relative percentage of each cluster
+        endX = startX + (percent * 36)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 12),
+                      color.astype("uint8").tolist(), -1)
+        startX = endX
+    return bar
+
+
+bar = plot_colors(hist, model.cluster_centers_)
+
+# 중복값 제거
+mylist = bar[0]
+
+# 비율 rgb 담기 위한 my_lists
+mylists = []
+for a, b, c in mylist:
+    if [a, b, c] not in mylists:
+        mylists.append([a, b, c])
+
+
+# 옷의 밝기 인식을 위한 명도 구하기
+h1, s1, v1 = colorsys.rgb_to_hsv(
+    mylists[max_index][0]/255, mylists[max_index][1]/255, mylists[max_index][2]/255)
+
+print("명도", v1)
+# 밝음
+if (v1 > 0.5):
+    flag = 1
+# 어두움
+else:
+    flag = 0
+
+print("밝은 옷: 1 어두운 옷: 0 =>", flag)
+# show our color bartS
+plt.figure()
+plt.axis("off")
+plt.imshow(bar)
+plt.show()
+
+
 
--- rename.py
+++ rename.py
@@ -2,8 +2,8 @@ import os
 import glob
 import shutil
 
-path = 'dataset/dataset/pantie'
-save_path = 'dataset/dataset/pantie/'
+path = 'dataset/dataset/pants'
+save_path = 'dataset/dataset/pants/'
 
 cnt = len(os.listdir(save_path))
 files = glob.glob(path + '/*')
@@ -13,7 +13,7 @@ for i, f in enumerate(files):
     fname = ftitle.split('_')[0]  # 파일이름
 
     # 파일이름을 fname_0000.jpg 이런 식으로 변경
-    filename = save_path + 'pantie' + '_' + '{0:04d}'.format(cnt) + fext
+    filename = save_path + 'pants' + '_' + '{0:04d}'.format(cnt) + fext
     print(filename)
 
     # Use shutil.move to overwrite existing files
