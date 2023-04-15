2194ddac99de260190bc992403d374a32d058aa5
seokahi <skh9797@naver.com>
2023-04-15 16:10:01 +0900

    feat : 이미지 각도 추가

--- rotateImg.py
+++ rotateImg.py
@@ -1,6 +1,10 @@
 from PIL import Image
 import os
 
+input_folder = 'C:/Python/OpenCV/AutoCrawler-master/download/socks'  # 입력 폴더 경로
+output_folder = 'C:/Python/OpenCV/AutoCrawler-master/download/socks-ouput2'  # 출력 폴더 경로
+
+
 def rotate_images(input_folder, ouput_folder, degrees):
     for filename in os.listdir(input_folder):
         if filename.endswith(".jpg") or filename.endswith(".png"):
@@ -12,10 +16,13 @@ def rotate_images(input_folder, ouput_folder, degrees):
                 output.save(output_path)
 
 # 폴더 내의 모든 이미지 회전시키기
-input_folder = 'C:/Python/OpenCV/AutoCrawler-master/download/socks'  # 입력 폴더 경로
-output_folder = 'C:/Python/OpenCV/AutoCrawler-master/download/socks-ouput2'  # 출력 폴더 경로
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-degrees = 15
+degrees = 90
 rotate_images(input_folder, output_folder, degrees)
+degrees = 180
+rotate_images(input_folder, output_folder, degrees)
+degrees = 270
+rotate_images(input_folder, output_folder, degrees)
+print(len(os.listdir(output_folder)))
