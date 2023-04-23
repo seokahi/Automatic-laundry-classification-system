d338f56981aac1647d2beba350c29dd18c52c080
seokahi <skh9797@naver.com>
2023-04-23 11:10:01 +0900

    feat : 카테고리별 라벨 생성

--- make_H5)file.py
+++ make_H5)file.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
+
 train_set = tf.keras.utils.image_dataset_from_directory(directory='/content/drive/MyDrive/cap/custom_dataset/',
                                         # rescale=1./255,
                                         image_size=(28,28),
@@ -25,3 +26,21 @@ valid_set = tf.keras.utils.image_dataset_from_directory(directory='/content/driv
                                         batch_size=32)
 image_data = tf.keras.utils.image_dataset_from_directory(directory='/content/drive/MyDrive/cap/custom_dataset/',
                                          image_size=(28,28))
+
+label = ['bra', 'pantie', 'pants', 'shirt',
+               'short pants', 'skirt', 'socks', 't-shirt']
+
+idx = 0
+class_label = dict()
+for i in label:
+  class_label[i] = idx
+  idx += 1
+class_label
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_set.take(1):
+  for i in range(25):
+    ax = plt.subplot(5, 5, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(train_set.class_names[labels[i]])
+    plt.axis("off")
