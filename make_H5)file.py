29706fa63a972fcf21a4f5224072a398d676ec3b
seokahi <skh9797@naver.com>
2023-04-24 15:12:01 +0900

    feat : h5 파일 생성 위한 cnn 코드 추가

--- make_H5)file.py
+++ make_H5)file.py
@@ -44,3 +44,21 @@ for images, labels in train_set.take(1):
     plt.imshow(images[i].numpy().astype("uint8"))
     plt.title(train_set.class_names[labels[i]])
     plt.axis("off")
+
+model = keras.Sequential([
+    keras.layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(28, 28, 1)),
+    keras.layers.MaxPooling2D((2, 2)),
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.MaxPooling2D((2, 2)),
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.Flatten(),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
