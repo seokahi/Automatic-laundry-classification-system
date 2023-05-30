1b84d745d5bbfd4e4a455d87331c98432a25fbf8
seokahi <skh9797@naver.com>
2023-05-30 22:12:01 +0900

    chohre : 코드 수정 & 주석 제거

--- main_raspberryPi.py
+++ main_raspberryPi.py
@@ -12,7 +12,7 @@ import glob
 import RPi.GPIO as GPIO
 port_name = glob.glob('/dev/ttyACM*')
 print(port_name)
-ser = serial.Serial(port_name[0], 9600, timeout=1) # 아두이노와 시리얼 통신 ser설정
+ser = serial.Serial(port_name[0], 9600, timeout=1)
 button_pin = 15
 
 GPIO.setwarnings(False)
@@ -25,8 +25,8 @@ time.sleep(1)
 while True:
     time.sleep(0.1)
     
-    if GPIO.input(button_pin) == GPIO.HIGH: # 버튼이 눌리면
-        ####
+    if GPIO.input(button_pin) == GPIO.HIGH: 
+
         camera = cv2.VideoCapture(0)
         ret, frame = camera.read()
         if camera.isOpened():
@@ -34,9 +34,9 @@ while True:
             
         cv2.imwrite('result_image/image.jpg',frame)
         imageUrl = 'result_image/image.jpg'
-        print('Image captured') # 이미지 캡처 완료 메시지 출력
+        print('Image captured') 
 
-        frame = [] # frame =0
+        frame = [] 
         img = cv2.imread(imageUrl)
         ori_img = img
         start = time.time()
@@ -52,7 +52,6 @@ while True:
         image_gray = cv2.imread('result_img/img.jpg', cv2.IMREAD_GRAYSCALE)
         image_gray = cv2.resize(image_gray, dsize=(0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
         cv2.imwrite('result_img/gray.jpg', image_gray)
-        # image_gray = cv2.normalize(image_gray, None, 0, 255, cv2.NORM_L1)
         image_gray = cv2.equalizeHist(image_gray)
         cv2.imwrite('result_img/gray_nor.jpg', image_gray)
         image = img[:290, 0:545]
@@ -121,9 +120,7 @@ while True:
 
         cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
         print("ROI selection and grabCut processing part: ", time.time() - start)
-        ####
         start = time.time()
-        # cv2.grabCut(img, mask, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
 
         mask2 = np.where((mask == cv2.GC_FGD) | (
             mask == cv2.GC_PR_FGD), 255, 0).astype('uint8')
@@ -136,7 +133,6 @@ while True:
         mask3 = cv2.inRange(result2, np.array([0, 0, 0]), np.array([8, 8, 8]))
         result2[mask3 == 255] = (255, 255, 255)
         
-        ####################################################################################### k-means ?  고리 ? ################################################################################
         ## k-means
         image = cv2.imread('result2.png')
 
@@ -162,72 +158,47 @@ while True:
 
 
         hist = centroid_histogram(model)
-        print("?  ?   비율 출력 => ", hist)
 
-        #  ??   많 ?? 비율 차 ???   ? ?  ?   index 추출
         max_index = hist.argmax()
 
-        # bar 그려주기 ?  ?   ?  ?  
 
 
         def plot_colors(hist, centroids):
             bar = np.zeros((12, 36, 3), dtype="uint8")
             startX = 0
             for (percent, color) in zip(hist, centroids):
-                # plot the relative percentage of each cluster
                 endX = startX + (percent * 36)
                 cv2.rectangle(bar, (int(startX), 0), (int(endX), 12),
                             color.astype("uint8").tolist(), -1)
                 startX = endX
             return bar
 
-        #------------------------------------------------------------
         bar = plot_colors(hist, model.cluster_centers_)
 
-        # 중복 ? ?   ?
         mylist = bar[0]
 
-        # 비율 rgb ?   ? ?  ?   my_lists
         mylists = []
         for a, b, c in mylist:
             if [a, b, c] not in mylists:
                 mylists.append([a, b, c])
 
-
-        # ?  ?   밝기 ?  ?  ?   ?  ?   명도 구하 ?
         h1, s1, v1 = colorsys.rgb_to_hsv(
             mylists[max_index][0]/255, mylists[max_index][1]/255, mylists[max_index][2]/255)
 
         print("명도", v1)
-        # 밝음
         if (v1 > 0.5):
-            # flag = 1
             flag = "light"
-        # ?  ?  ???
         else:
-            # flag = 0
             flag= "dark"
 
         print("%s" %flag)
-        # show our color bartS
-        # plt.figure()
-        # plt.axis("off")
-        # plt.imshow(bar)
-        # plt.show()
-
-        ####################################################################################### k-means ?  고리 ? ################################################################################
-
-
-        ####################################################################################### fashion-mnist ?  고리 ? ################################################################################
         MNIST_model = tf.keras.models.load_model('/custom_final.h5', compile=False)
         img = tf.keras.preprocessing.image.load_img(
             './result2.png',
             target_size=(28, 28),
             color_mode='grayscale'
         )
-        # img_array = tf.keras.utils.img_to_array(img)
         img_array = tf.keras.preprocessing.image.img_to_array(img)
-        # img_array = tf.keras.preprocessing.img_to_array(img)
 
         image = np.expand_dims(img_array, axis=-1)
         image = np.expand_dims(img_array, axis=0)
@@ -242,9 +213,7 @@ while True:
         print(class_index)
         print('Predicted class:', class_names[class_index])
 
-        # plt.imshow(img)
-        # plt.show()
-
+   
         if (class_names[class_index] == "bra" or class_names[class_index] == "pantie"):
             print("under")
             ser.write(b'1\n')
@@ -256,5 +225,4 @@ while True:
             ser.write(b'3\n')
         
         GPIO.input(button_pin) == GPIO.LOW
-        ####################################################################################### fasjion-mnist ?  고리 ? ################################################################################
-        
+   
